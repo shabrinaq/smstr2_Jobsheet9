a4eb6cc0e586b735df84ecc43846a27ff26580e8
shabrinaq <shabrinaqottrunnada@gmail.com>
2024-05-19 21:34:09 +0700

    first commit

--- SLLMain21.java
+++ SLLMain21.java
@@ -15,7 +15,19 @@ public class SLLMain21 {
         singLL.print();
         singLL.insertAfter(700, 999);
         singLL.print();
-        singLL.insertAfter(3, 833);
+        singLL.insertAt(3, 833);
+        singLL.print();
+
+        System.out.println("Data in lst index : " + singLL.getData(1));
+        System.out.println("Data 3 is in index : " + singLL.indexOf(760));
+
+        singLL.remove(999);
+        singLL.print();
+        singLL.removeAt(0);
+        singLL.print();
+        singLL.removeFirst();
+        singLL.print();
+        singLL.removeLast();
         singLL.print();
     }
 }
--- SingleLinkedList21.java
+++ SingleLinkedList21.java
@@ -61,7 +61,7 @@ public class SingleLinkedList21 {
         } while (temp != null);
     }
 
-    public void insertAfterIndex(int index, int input) {
+    public void insertAt(int index, int input) {
         if (index < 0) {
             System.out.println("Wrong index");
         } else if (index == 0) {
@@ -73,6 +73,89 @@ public class SingleLinkedList21 {
             }
             temp.next = new Node21(input, temp.next);
             if (temp.next.next == null) tail = temp.next;
+        }
+    }
+
+    public int getData(int index) {
+        Node21 tmp = head;
+        for (int i = 0; i < index; i++) {
+            tmp = tmp.next;
+        }
+        return tmp.data;
+    }
+
+    public int indexOf(int key) {
+        Node21 tmp = head;
+        int index = 0;
+        while (tmp != null && tmp.data != key) {
+            tmp = tmp.next;
+            index++;
+        }
+
+        if (tmp == null) {
+            return -1;
+        } else {
+            return index;
+        }
+    }
+
+    public void removeFirst() {
+        if (isEmpty()) {
+            System.out.println("Linked list is empty. cannot remove a data");
+        } else if (head == tail) {
+            head = tail = null;
+        } else {
+            head = head.next;
+        }
+    }
+
+    public void removeLast() {
+        if (isEmpty()) {
+            System.out.println("Linked list is empty. cannot remove a data");
+        } else if (head == tail) {
+            head = tail = null;
+        } else {
+            Node21 temp = head;
+            while (temp.next != tail) {
+                temp = temp.next;
+            }
+            temp.next = null;
+            tail = temp;
+        }
+    }
+
+    public void remove(int key) {
+        if (isEmpty()) {
+            System.out.println("Linked list is empty. cannot remove a data");
+        } else {
+            Node21 temp = head;
+            while (temp != null) {
+                if (temp.data == key && temp == head) {
+                    this.removeFirst();
+                } else if (temp.next.data == key) {
+                    temp.next = temp.next.next;
+                    if (temp.next == null) {
+                        tail = temp;
+                    }
+                    break;
+                }
+                temp = temp.next;
+            }
+        }
+    }
+
+    public void removeAt(int index) {
+        if (index == 0) {
+            removeFirst();
+        } else {
+            Node21 temp = head;
+            for (int i = 0; i < index - 1; i++) {
+                temp = temp.next;
+            }
+            temp.next = temp.next.next;
+            if (temp.next == null) {
+                tail = temp;
+            }
         }
     }
 }
